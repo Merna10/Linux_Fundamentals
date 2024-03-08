92049688c7f073ff2ec923dfb0124f9adc296821
Merna10 <85399091+Merna10@users.noreply.github.com>
2024-03-09 01:02:21 +0200

    organize.sh

--- organize.sh
+++ organize.sh
@@ -13,7 +13,6 @@ organize_files(){
         if [ -f "$file" ]; then
             extension="${file##*.}"
             filename=$(basename "$file")
-            echo 
             if [[ "$extension" == "$file" || "$extension" == "unknown" ||  "${filename:0:1}" == "."  ]];  then
                 
                 if [ ! -d "$directory/misc" ]; then
